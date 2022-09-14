a8e254094c7bdb32bdf9e4d24c8bfce3d2b9a7a1
shashi kant <shashik024@gmail.com>
2022-09-14 19:32:50 +0530

    created__featured_page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header className="App-header">
         <a href="/">Bhagwati</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1> Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div key={product.slug} className="product">
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>₹ {product.price}</strong>
+                </p>
+                <button>Add to Cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
